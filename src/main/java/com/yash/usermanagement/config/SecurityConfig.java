07441a20313dc5d7532239a1a6813df36df73a75
Yashgupta2023 <yashguptav27@gmail.com>
2026-06-27 08:24:44 +0530

    implemented spring security with in-memory authentication and role-based authorization

---
+++ src/main/java/com/yash/usermanagement/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.yash.usermanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers("/api/posts/**")
+                        .permitAll()
+
+                        .requestMatchers("/api/users/**")
+                        .hasRole("ADMIN")
+
+                        .anyRequest()
+                        .hasAnyRole("ADMIN", "USER")
+
+                )
+
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin123")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user123")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+}
